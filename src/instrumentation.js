5924d17355ce56730ff547553d9ed6cadcc6abd8
Hardik Choksi <hardik@mw.io>
2025-08-06 12:48:15 +0530

    Enhance MiddlewareWebpackPlugin configuration in next.config.js and add custom resource attributes in instrumentation.js
    Updated MiddlewareWebpackPlugin to use environment variable for access token and added custom resource attributes for application version in instrumentation.js.

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -6,7 +6,10 @@ function register() {
         serviceName: "wrkm-nextjs",
         accessToken: process.env.MW_ACCESS_TOKEN || '',
         enableExceptionHandling: true, // Enable advanced exception handling
-        target: "vercel"
+        target: "vercel",
+        customResourceAttributes: {
+            "app.version": "2.0.0"
+        }
     });
 }
 
