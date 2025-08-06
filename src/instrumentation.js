38318c6a67ce3b438ad17f24b73904a70541bc9b
Hardik Choksi <hardik@mw.io>
2025-08-06 11:36:14 +0530

    Add target configuration for Vercel in instrumentation.js

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -6,6 +6,7 @@ function register() {
         serviceName: "wrkm-nextjs",
         // accessToken: "xusuusalpvush63ud7zcg8bi3mauuptds528",
         enableExceptionHandling: true, // Enable advanced exception handling
+        target: "vercel"
     });
 }
 
